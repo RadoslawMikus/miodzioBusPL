41d8131144e8cb5c8e9dd260b5dd3e22af3fbd83
davidsmith931 <radoslaw.mikus@gmail.com>
2022-05-01 20:26:00 +0200

    Fixed responsitivity

--- script/main.js
+++ script/main.js
@@ -7,3 +7,16 @@ const scroll = new SmoothScroll('.navbar a[href*="#"]', {
   speedAsDuration: true,
   header: "[data-scroll-header]",
 });
+
+// $(window).scroll(function(e) {
+
+//   var scroll = $(window).scrollTop();
+//   if (scroll >= 150) {
+//       $('.navbar').addClass("navbar-hide");
+//       $('.logo').addClass("logo-hide");
+//   } else {
+//       $('.navbar').removeClass("navbar-hide");
+//       $('.logo').removeClass("logo-hide");
+//   }
+
+// });
