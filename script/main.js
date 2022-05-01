e600a00ab477c2026d66f32f530def3d46512768
davidsmith931 <radoslaw.mikus@gmail.com>
2022-05-01 18:53:37 +0200

    Added Contact

--- script/main.js
+++ script/main.js
@@ -1,5 +1,7 @@
 // POLYFILL SCROLL
 // -----------------------------
+
+document.querySelector("html").style.scrollBehavior = "auto";
 const scroll = new SmoothScroll('.navbar a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
